6aeffa08fc0f54c93b61d7cf7c51781dd7381d4c
mteam88 <matthew8edelen@gmail.com>
2022-09-23 17:52:01 +0000

    implement key generation in main.go

--- main.go
+++ main.go
@@ -3,24 +3,24 @@ package main
 import (
 	"context"
 	"crypto/rand"
+	"encoding/hex"
 	"fmt"
-	"math/big"
+	"log"
 	mathrand "math/rand"
 	"net/http"
 	"os"
-	"os/exec"
 	"strings"
 	"sync"
 	"time"
-	"log"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 // config
-const HQ="http://localhost:8000/"
+const HQ = "http://localhost:8000/"
 const producerCount int = 8
 const minimumBalanceWei int = 1
 const reportSpeed int = 10
@@ -33,14 +33,13 @@ func main() {
 	ETHProviders = loadETHProviders()
 	genkeys := make(chan []string)
 	keyswithbalance := make(chan []string)
-	done := make(chan bool)
 	wg := sync.WaitGroup{}
 
 	go func() {
 		for {
-			log.Default().Println("[$] Keys Per Second: ", (scannedkeys/reportSpeed))
+			log.Default().Println("[$] Keys Per Second: ", (scannedkeys / reportSpeed))
 			scannedkeys = 0
-			time.Sleep(time.Second*time.Duration(reportSpeed))
+			time.Sleep(time.Second * time.Duration(reportSpeed))
 		}
 	}()
 
@@ -49,25 +48,27 @@ func main() {
 		go generatekeys(genkeys, keyswithbalance, i, &wg)
 	}
 
-	go callhome(keyswithbalance, done)
+	go callhome(keyswithbalance)
 
 	wg.Wait()
 	close(keyswithbalance) // should never happen
-	<-done
 }
 func generatekeys(generatedkeys chan []string, keyswithbalance chan []string, idx int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	go filterforbalance(generatedkeys, keyswithbalance)
 	for {
-		page := new(big.Int)
-		page.SetBytes(generateRandomBytes(249/8))
-		// fmt.Println("[:] Scanning page: ", page)
-
-		out, err := exec.Command("./xkeygen", "eth", page.String()).Output()
+		// Create an account
+		key, err := crypto.GenerateKey()
 		if err != nil {
 			panic(err)
 		}
-		go parsexkeygen(out, generatedkeys)
+
+		// Get the address
+		address := crypto.PubkeyToAddress(key.PublicKey).Hex()
+
+		// Get the private key
+		privateKey := hex.EncodeToString(key.D.Bytes())
+		generatedkeys <-[]string{privateKey, address}
 	}
 }
 
@@ -81,11 +82,10 @@ func filterforbalance(generatedkeys chan []string, keyswithbalance chan []string
 	}
 }
 
-func callhome(keyswithbalance <-chan []string, done chan<- bool) {
+func callhome(keyswithbalance <-chan []string) {
 	for keypair := range keyswithbalance {
 		beacon(keypair)
 	}
-	done <- true
 }
 
 func hasbalance(keypair []string) bool {
@@ -102,58 +102,37 @@ func getbalance(keypair []string) (int, error) { //returns wei balance of keypai
 	var ethprovider ETHProvider
 	for {
 		ethprovider = ETHProviders[mathrand.Intn(len(ETHProviders))]
-		if (!ethprovider.isMax) {
+		if !ethprovider.isMax {
 			break
 		}
 	}
 	client, err := ethclient.Dial(ethprovider.RawURL)
-    if err != nil {
-        return -1, err
-    }
+	if err != nil {
+		return -1, err
+	}
 
-    account := common.HexToAddress(keypair[1])
-    balance, err := client.BalanceAt(context.Background(), account, nil)
-    if err != nil {
-		if (strings.Contains(err.Error(), "429")) {
+	account := common.HexToAddress(keypair[1])
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		if strings.Contains(err.Error(), "429") {
 			ethprovider.isMax = true
 		}
-        return -1, err
-    }
+		return -1, err
+	}
 	return int(balance.Int64()), nil
 }
 
-func generateRandomBytes(n int) ([]byte) {
-
-    b := make([]byte, n)
-    _, err := rand.Read(b)
-    
-    if err != nil {
-        return nil
-    }
-
-    return b
-}
-
 func beacon(keypair []string) {
-	_, err := http.Get(HQ+keypair[0])
+	_, err := http.Get(HQ + keypair[0])
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println("[!] BEACON CALL: " +"\n[-] Private: "+ keypair[0] +"\n[-] Public: "+ keypair[1])
-}
-
-func parsexkeygen(out []byte, outch chan []string) {
-	entries := strings.Split(string(out), "\n")
-	for _, entry := range entries {
-		entry = strings.ReplaceAll(entry, "{", "")
-		entry = strings.ReplaceAll(entry, "}", "")
-		outch <-strings.Split(entry, " ")
-	}
+	fmt.Println("[!] BEACON CALL: " + "\n[-] Private: " + keypair[0] + "\n[-] Public: " + keypair[1])
 }
 
 type ETHProvider struct {
 	RawURL string
-	isMax bool
+	isMax  bool
 }
 
 func loadETHProviders() []ETHProvider {
