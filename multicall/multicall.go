ef6dc59c94066034ddbd866ca590eee045b323d2
mteam88 <84196639+mteam88@users.noreply.github.com>
2022-09-29 15:38:00 +0000

    .

--- multicall/multicall.go
+++ multicall/multicall.go
@@ -3,9 +3,10 @@ package multicall
 import (
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	_ "github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 var multicallContractAddress = common.HexToAddress("0x5e227AD1969Ea493B43F840cfF78d08a6fc17796")
@@ -26,7 +27,7 @@ func GetBalances(addresses []string, ETHProviderURL string) ([]string, error) {
 	var calls = []MulticallCall{}
 	for _, address := range addresses {
 		hashAddress := common.HexToHash(address)
-		call := MulticallCall{multicallContractAddress, []byte("0x"+multicallContractEthBalanceSelector+hashAddress.String()[2:])}
+		call := MulticallCall{multicallContractAddress, []byte("0x" + multicallContractEthBalanceSelector + hashAddress.String()[2:])}
 		calls = append(calls, call)
 	}
 
@@ -34,7 +35,7 @@ func GetBalances(addresses []string, ETHProviderURL string) ([]string, error) {
 
 	var results []byte
 
-	err = multicallContract.contract.Call(&bind.CallOpts{}, nil, "aggregate", common.Hex2Bytes(addresses[0]))
+	//err = multicallContract.contract.Call(&bind.CallOpts{}, results, "aggregate", common.Hex2Bytes(addresses[0]))
 	if err != nil {
 		panic(err)
 	}
